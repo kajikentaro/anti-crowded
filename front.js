bc7dd8c9429c76a55d13ceed8f475b776caa83d9
ryota217 <85782874+ryota217@users.noreply.github.com>
2021-06-19 17:36:02 +0900

    refactor js files

--- front.js
+++ front.js
@@ -1,20 +1,46 @@
 var socketio = io();
-    $(function(){
-        socketio.on('message',function(msg){
-            let data=msg
-            console.log(data)
-            if(data.count>=3){
-                change_color(data.id + "","#ff0000")
-                console.log(data.id)
-              }else{
-                console.log(data.count)
-                  console.log("test")
-              }
-          });
-        });
-    function change_color(id,color_code){
-        var poly = document.getElementById(id);
-        console.log(poly)
-        poly.setAttribute("fill", color_code);
+$(function () {
+    socketio.on('message', function (msg) {
+        let data = msg
+        console.log(data)
+        if (data.count >= 3) {
+            change_color(data.id + "", "#ff0000")
+            console.log(data.id)
+        } else {
+            console.log(data.count)
+            console.log("test")
+        }
+    });
+});
+function change_color(id, color_code) {
+    var poly = document.getElementById(id);
+    console.log(poly)
+    poly.setAttribute("fill", color_code);
     //poly.setAttribute("stroke", "black");
-  }
+}
+
+$(function () {
+    console.log("a")
+    var rexts = document.getElementsByTagName("rect")
+    async function post(data) {
+        const url = "https://anti-crowded.com/api";
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+        const res = await fetch(url, options)
+        return await res.json()
+    }
+    let res = await post({ "type": "get-rooms" })
+    for (let i = 0; i < res.length; i++) {
+        for (let j = 0; j < rexts.length; j++) {
+            if (res[i].id == rexts[j].id) {
+                console.log(res[i])
+                console.log(rexts[j])
+            }
+        }
+    }
+})
--- mybatch - コピー.bat
+++ mybatch - コピー.bat
@@ -1,4 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d "{\"type\":\"create-room\", \"value\":1,\"id\":5235, \"name\":\"hello\"}" http://localhost:3000/api
-curl -X POST -H "Content-Type: application/json" -d "{\"type\":\"update-room\", \"value\":1,\"id\":5235, \"name\":\"hello\"}" http://localhost:3000/api
-curl -X POST -H "Content-Type: application/json" -d "{\"type\":\"update-room\", \"value\":1,\"id\":5235, \"name\":\"hello\"}" http://localhost:3000/api
-curl -X POST -H "Content-Type: application/json" -d "{\"type\":\"update-room\", \"value\":1,\"id\":5235, \"name\":\"hello\"}" http://localhost:3000/api
+curl -X POST -H "Content-Type: application/json" -d "{\"type\":\"create-room\", \"id\":5235, \"name\":\"hello\", \"green_max\":5235, \"yellow_max\":5235 }" http://localhost:3000/api
