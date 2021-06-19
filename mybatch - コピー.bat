e0e49304364ead096a85392f3a1a0bc2b31007c1
ryota217 <85782874+ryota217@users.noreply.github.com>
2021-06-19 22:12:49 +0900

    update floor3.html floor5.html

--- mybatch - コピー.bat
+++ mybatch - コピー.bat
@@ -1,1 +1,5 @@
-curl -X POST -H "Content-Type: application/json" -d "{\"type\":\"create-room\", \"id\":5235, \"name\":\"hello\", \"green_max\":5235, \"yellow_max\":5235 }" http://localhost:3000/api
+type:"create-room", "id":5235, "name":"hello", "green_max":5235, "yellow_max":5235, "count":0
+
+
+
+type:"create-room", "id":career, "name":"hello", "green_max":5235, "yellow_max":5235, "count":0
